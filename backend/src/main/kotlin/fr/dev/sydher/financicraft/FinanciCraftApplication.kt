dc260caa9afbeec8191c4a783e2db70feb9a2e70
Sydher <hugoc.pro@gmail.com>
2023-12-31 14:48:33 +0100

    Init backend

---
+++ backend/src/main/kotlin/fr/dev/sydher/financicraft/FinanciCraftApplication.kt
@@ -0,0 +1,11 @@
+package fr.dev.sydher.financicraft
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanciCraftApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanciCraftApplication>(*args)
+}
